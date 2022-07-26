2bd2e4b7ad00c075ebdeb8c7e31e3c045f603272
ml-laure <me.mlaure@gmail.com>
2022-07-26 17:26:23 +0100

    style scrollbar, add theme styles to head

--- app.vue
+++ app.vue
@@ -11,10 +11,12 @@ useHead({
       href: '/favicon.svg',
     },
   ],
+  style: [{ type: 'text/css', children: `html{color:#eee;background:#111;}` }],
 });
 </script>
 
 <template>
+  <TheHeader />
   <main id="layout-main">
     <NuxtLoadingIndicator />
     <NuxtPage />
@@ -26,16 +28,43 @@ html,
 body,
 #__nuxt {
   height: 100vh;
-  margin: 0;
-  padding: 0;
   color-scheme: dark;
   color: hsl(0, 0%, 75%);
-  font-family: 'Signika Negative', system-ui, -apple-system, Ubuntu, sans-serif;
+  font-family: 'Merriweather Sans', system-ui, -apple-system, Ubuntu, sans-serif;
+
+  @apply outline-none m-0 p-0;
 }
 
 html {
   overflow-y: scroll;
   background: @blk-1;
+}
+
+* {
+  scrollbar-color: rgb(17, 17, 17) rgb(5, 5, 5);
+}
+
+::-webkit-scrollbar {
+  width: 6px;
+
+  &:horizontal {
+    height: 6px;
+  }
+
+  &-track,
+  &-corner {
+    background: #050505;
+    border-radius: 10px;
+  }
+
+  &-thumb {
+    background: #111;
+    border-radius: 10px;
+
+    &:hover {
+      background: #222;
+    }
+  }
 }
 
 #layout-main {
